1e17e79f2a605fe66b05ad23f2e57e8d7f7d8799
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-09-14 10:46:29 +0200

    Poprawione rozwiązanie zadania 6.1

--- src/pl/altkom/zad6_1/LeapYearMain.java
+++ src/pl/altkom/zad6_1/LeapYearMain.java
@@ -6,16 +6,12 @@ public class LeapYearMain {
     public static boolean isLeapYear(int year) {
         if (year % 400 == 0) {
             return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
         } else {
-            if (year % 100 == 0) {
-                return false;
-            } else {
-                if (year % 4 == 0) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+            return false;
         }
     }
 
