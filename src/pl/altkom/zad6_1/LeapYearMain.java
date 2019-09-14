fcc961aed5d97e5cd8a12cf338f1ff8020f11f7b
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-09-14 10:42:48 +0200

    Początkowe rozwiązanie zadania 6.1

---
+++ src/pl/altkom/zad6_1/LeapYearMain.java
@@ -0,0 +1,29 @@
+package pl.altkom.zad6_1;
+
+import java.util.Scanner;
+
+public class LeapYearMain {
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else {
+            if (year % 100 == 0) {
+                return false;
+            } else {
+                if (year % 4 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int year = scanner.nextInt();
+            System.out.println(isLeapYear(year));
+        }
+    }
+}
